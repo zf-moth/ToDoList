eff21ab461c10909f690876d44789a8b59828c2a
MotH <mir0shi@pm.me>
2023-02-28 12:33:33 +0100

    Added TodoItem Class

---
+++ TodoItem.cs
@@ -0,0 +1,21 @@
+﻿namespace ToDoList
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public string? Description { get; set; }
+        Priority Priority { get; set; }
+        DateTime? DeadlineDate { get; set; }
+        DateTime CreatedDate { get; set; }
+        DateTime? FinishedDate { get; set; }
+
+        public TodoItem(int id, string title, DateTime createdDate)
+        {
+            Id = id;
+            Title = title;
+            CreatedDate = createdDate;
+        }
+    }
+    public enum Priority { normal, low, high, urgent }
+}
